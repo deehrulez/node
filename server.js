ffc424817cc1c409a434b201cc8ec83658ec6a92
Andre Sarmento <alsobox@hotmail.com>
2019-12-11 14:22:47 -0300

    mongo e conectando db

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
+// Iniciando o App
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello World");
-}); 
+// Iniciando o DB
+mongoose.connect(
+    "mongodb://localhost:27017/nodeapi", 
+    { useNewUrlParser: true }
+    );
+requireDir("./src/models");
+
+const Product = mongoose.model('Product');
+1
+// Primeira Rota
+app.get("/", (req, res) => {
+    Product.create({
+        title: 'React Native',
+        description: 'Build native apps with React',
+        url: 'http://github.com/facebook/react-native'
+    });
+
+    return res.send("Hello World");
+});
 
 app.listen(3001);
